2e6be8ef1943c75fe9636ea5404084ac64e34ec5
fast2281 <fasttop1gg@yandex.ru>
2024-05-24 13:34:47 +0300

    script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function gameLoop() {
             placeFood(); // После съедания добавляем новое яблоко
         }
         drawEverything();
-        setTimeout(gameLoop, 45);
+        setTimeout(gameLoop, 50);
     }
 }
 
