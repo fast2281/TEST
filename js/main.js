a14f2e29c74138398623392a172d68a86bde9213
ArtTov <arttov31@gmail.com>
2024-05-27 14:29:07 +0300

    0

---
+++ js/main.js
@@ -0,0 +1,4 @@
+document.getElementById('startButton').addEventListener('click', function () {
+    // Перенаправляем пользователя на страницу выбора уровня
+    window.location.href = 'levelSelection.html'; // <-- Замените на нужный URL страницы выбора уровня
+});
