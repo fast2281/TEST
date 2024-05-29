8f72c6973ef20087dfab72f19d1825c3ed4e0120
fast2281 <fasttop1gg@yandex.ru>
2024-05-29 14:22:00 +0300

    Add files via upload

---
+++ js/levelSelection.js
@@ -0,0 +1,11 @@
+function startLevel(level) {
+    if (level === 0) {
+        window.location.href = 'lvl0.html';
+    } else if (level === 1) {
+        window.location.href = 'lvl1.html';
+    }
+}
+
+function goBack() {
+    window.history.back();
+}
